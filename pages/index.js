27acd2db28a65bd4007f86fafc64568c7bf96f8f
Muhammad Syahman <muhammadsyahman@gmail.com>
2022-12-18 16:22:56 +0800

    minor title fix

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-stone-100 font-inter">
             <div className="flex select-none flex-col text-center text-6xl font-bold">
-                <span className="mb-4">{APP_NAME}</span>
+                <span className="mb-4">PermaWeb</span>
                 <Link
                     className="m-5 mx-auto rounded-full bg-green-400 py-3 px-5 text-3xl shadow-xl active:shadow-sm"
                     href="/uploader"
